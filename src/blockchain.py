5766b19baf0ebbf6182dfb164df9fd594b6c5aa1
lonestarx1 <siradriel@gmail.com>
2021-12-04 18:30:31 +0900

    adding a method to register a block

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,4 +1,6 @@
 # Blockchain build in python
+import time
+
 
 class Blockchain:
 
@@ -6,11 +8,21 @@ class Blockchain:
         self.chain = []
         self.pending_transactions = []
     
-    def registerBlock(self):
+    def registerBlock(self, proof, previous_hash=None):
         '''
-        Creates a new block and append it to the chain
+        - Creates a new block, append it to the chain and reset pending transactions
+        - Returns the newly created block
         '''
-        pass
+        block = {
+            'indx': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.pending_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]), # todo: check is re-calculating the hash always is better
+        }
+        self.chain.append(block)
+        self.pending_transactions = []
+        return block
 
 
     def registerTransaction(self, sender, receiver, amount):
