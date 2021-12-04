30de6a0399bf906420bd19a542fca78c5b583ae4
lonestarx1 <siradriel@gmail.com>
2021-12-04 20:57:25 +0900

    fixing bugs in gen block generation and block registration

--- src/blockchain.py
+++ src/blockchain.py
@@ -6,39 +6,49 @@ import json
 class Blockchain:
 
     def __init__(self):
+        '''
+        - Initialize the blockchain with a genesis block
+        '''
         self.chain = []
         self.pending_transactions = []
-        # genesis block
-        self.registerBlock(proof=100, previous_hash=1)
+        genesis_block = {
+            'indx': len(self.chain) + 1,
+            'timestamp': time.time(),
+            'transactions': [],
+            'previous_hash':  1,
+            'nonce': 100
+        }
+        self.registerBlock(genesis_block)
+
     
-    def registerBlock(self, proof, previous_hash=None):
+    def generateBlock(self):
         '''
-        - Creates a new block, append it to the chain and reset pending transactions
-        - Returns the newly created block
+        - Creates a new block from pending transactions and return it to the caller
         '''
         block = {
             'indx': len(self.chain) + 1,
             'timestamp': time.time(),
             'transactions': self.pending_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]), # todo: check is re-calculating the hash always is better
+            'previous_hash':  self.hash(self.chain[-1]),
             'nonce': 0
         }
-        self.chain.append(block)
-        self.pending_transactions = []
         return block
 
+    
+    def registerBlock(self, block):
+        '''
+        - Register a new block in the chain
+        - Reset the list of pending transactions
+        '''
+        self.chain.append(block)
+        self.pending_transactions = []
 
-    def registerTransaction(self, sender, receiver, amount):
+
+    def registerTransaction(self, transaction):
         '''
         - Adds a new transaction to the list of transactions pending to be converted into a block
         - Returns the index of the block that will be containing this transaction
         '''
-        transaction = {
-            'sender': sender,
-            'receiver': receiver,
-            'amount': amount
-        }
         self.pending_transactions.append(transaction)
         return self.chain[-1]['indx'] + 1
 
@@ -60,4 +70,4 @@ class Blockchain:
         '''
         while self.hash(block)[:4] != '0000':
             block['nonce'] += 1
-        return block['nonce']
+        return block
