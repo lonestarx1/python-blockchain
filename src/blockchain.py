78120f0751555a65a63052aa0993da548d7ef0ea
lonestarx1 <siradriel@gmail.com>
2021-12-10 02:32:27 +0900

    supporting multiple nodes

--- src/blockchain.py
+++ src/blockchain.py
@@ -7,6 +7,9 @@ from urllib.parse import urlparse
 
 class Blockchain:
 
+    # in practice we would discover seed nodes though a DNS service
+    seed_node_address =  '61.73.43.24:5000'
+
     def __init__(self, mining_difficulty):
         '''
         - Initialize the blockchain with a genesis block
@@ -14,9 +17,9 @@ class Blockchain:
         self.mining_difficulty = mining_difficulty
         self.chain = []
         self.pending_transactions = []
-        self.nodes = set()
+        self.nodes = set(['61.73.43.24:5000'])
         genesis_block = {
-            'indx': len(self.chain) + 1,
+            'indx': 0,
             'timestamp': time.time(),
             'transactions': [],
             'previous_hash':  1,
@@ -41,11 +44,23 @@ class Blockchain:
     
     def registerBlock(self, block):
         '''
-        - Register a new block in the chain
-        - Reset the list of pending transactions
+        - add a new block to the chain
+        - if the resulting chain is valid save it, else undo it
         '''
+        # before we may try appending a block, we have to get the longest valid chain on the network
+        self.reachConsensus()
+
+        # try appending the block to the current chain
         self.chain.append(block)
-        self.pending_transactions = []
+
+        # if valid we win
+        if self.isValidChain(self.chain):
+            self.pending_transactions = []
+            return True
+        
+        # else undo what we added
+        self.chain.pop()
+        return False
 
 
     def registerTransaction(self, transaction):
@@ -74,6 +89,7 @@ class Blockchain:
         '''
         while True:
             cur_hash = self.hash(block)
+            # uncomment the next line to see proof-of-work in action
             print(cur_hash)
             if cur_hash[:self.mining_difficulty] == '0' * self.mining_difficulty:
                 break
@@ -83,43 +99,44 @@ class Blockchain:
 
     def registerNode(self, node_address):
         '''
-        - Add a new node to the list of nodes (address ex: http://128.123.0.5:5000) -> netloc:128.123.0.5:5000
-        - Returns True if the node is not already in the list
+        - Add a new node to the list of nodes 
         '''
-        parsed_url = urlparse(node_address)
-        self.nodes.add(parsed_url.netloc)
+        self.nodes.add(node_address)
 
     
-    def isValidChain(self, chain):
+    def isValidChain(self, received_chain=None):
         '''
         - Checks is the given chain is valid.
         - Returns True if the chain is valid, False otherwise
         '''
-        prev_block = self.chain[0]
+        chain = received_chain or self.chain
+        prev_block = chain[0]
         cur_block_indx = 1
-        while cur_block_indx < len(self.chain):
-            # check if its "prev_hash" is the correct hash of the previous block
-            if self.chain[cur_block_indx]['previous_hash'] != self.hash(prev_block):
+        while cur_block_indx < len(chain):
+            # check if its "prev_hash" is the correct hash of the previous block (except if prev block is genesis block)
+            if prev_block['indx'] > 0 and  chain[cur_block_indx]['previous_hash'] != self.hash(prev_block):
                 return False
             # check if the proof of work has been done correctly for cur_block
-            cur_block_hash = self.hash(self.chain[cur_block_indx])
+            cur_block_hash = self.hash(chain[cur_block_indx])
+            
             if cur_block_hash[:self.mining_difficulty] != "0" * self.mining_difficulty:
                 return False
             # if all checks out advance to the next block
-            prev_block = self.chain[cur_block_indx]
+            prev_block = chain[cur_block_indx]
             cur_block_indx += 1
         return True
+
 
     def reachConsensus(self):
         '''
         - A consensus algorithm that replaces the current node's chain with the longest valid chain on the network
         - visits all nodes on the network, download their chain, verifies them and keep the longest
         '''
-        nodes_on_network = self.nodes
+        print("GETTING THE LONGEST VALID CHAIN ON THE NETWORK")
         longest_valid_chain = self.chain
 
         # verify the chains from all nodes
-        for node in nodes_on_network:
+        for node in self.nodes:
             try:
                 res = requests.get(f'http://{node}/chain')
                 if res.status_code != 200:
@@ -132,5 +149,6 @@ class Blockchain:
             if length > len(longest_valid_chain) and self.isValidChain(chain):
                 longest_valid_chain = chain
             
-            # replace the current node's chain with the winner
-            self.chain = longest_valid_chain
+        # replace the current node's chain with the winner
+        self.chain = longest_valid_chain
+        print(f"CURRENT CHAIN HAS LENGTH {len(self.chain)}")
--- src/server.py
+++ src/server.py
@@ -1,3 +1,6 @@
+import socket
+import requests
+import sys
 import json
 from flask import Flask, jsonify, request
 import uuid
@@ -8,40 +11,111 @@ from blockchain import Blockchain
 app = Flask(__name__)
 node_id = str(uuid.uuid4()).replace('-', '')
 
+# current node's address
+host_ip = socket.gethostbyname(socket.gethostname())
+port = sys.argv[1] if len(sys.argv) > 1 else 5000
+host_address =  host_ip + ':' + str(port)
+
 
 # Instantiate the Blockchain
 blockchain = Blockchain(mining_difficulty=3)
-print("Blockchain initiated")
-print("Current Ledger:", blockchain.chain)
+
+# Registering a new node on the network
+
+if host_address != blockchain.seed_node_address:
+    
+    # Get addresses of the nodes on the network from the seed node
+    try:
+        res = requests.get(f'http://{blockchain.seed_node_address}/nodes')
+        if res.status_code != 200:
+            pass
+        node_addresses = res.json()['nodes']
+        for address in node_addresses:
+            blockchain.nodes.add(address)
+    except:
+        pass
+
+    # announce & register my address to all other nodes
+    for address in blockchain.nodes:
+        try:
+            requests.post(f'http://{address}/nodes/register', data=json.dumps({'nodes': [host_address,]}), headers={'Content-Type': 'application/json'})
+        except:
+            pass
+    
+    # register my own address as well
+    blockchain.nodes.add(host_address)
+
 
 @app.route('/mine', methods=['GET'])
 def mine():
     '''
-    - Generates a block from pending transactions
-    - Runs the proof of work algorithm to find the correct nonce (hence validating the block)
-    - Adds the block to the chain
-    - Rewards the miner by adding a transaction granting the miner a coin
+    - if seed node:forward the mining request to all other nodes on the nwtwork
+    - else: try mining the block
     '''
-    non_validated_new_block = blockchain.generateBlock()
-    print("New block generated:", non_validated_new_block)
 
-    validated_new_block     = blockchain.proofOfWork(non_validated_new_block)
-    print("New block validated:", validated_new_block)
+    
+    if host_address == blockchain.seed_node_address:
+        print('I AM SEED NODE, FORWORDING TO PARTICIPANTS')
+        # forward the mining request to all other nodes on the network
+        for address in blockchain.nodes:
+            if address == host_address:
+                continue
+            try:
+                requests.get(f'http://{address}/mine')
+            except:
+                pass
+        return jsonify({'message': 'Mining request forwarded to all nodes on the network'}), 200
+    else:
+        print('I AM A PARTICIPANT NODE, MINING THE BLOCK')
 
-    blockchain.registerBlock(validated_new_block)
-    reward_transaction = {
-        'sender': 'MINING',
-        'receiver': node_id,
-        'amount': 1
-    }
-    blockchain.registerTransaction(reward_transaction)
-    block_index = validated_new_block['indx']
+        # if no pending transactions there is no work to do
+        if len(blockchain.pending_transactions) == 0:
+            print("NO PENDING TRANSACTIONS")
+            return jsonify({'message': 'No transactions to mine'}), 200
+
+        # generate a block from pending transactions
+        non_validated_new_block = blockchain.generateBlock()
+       
+        # run the proof of work algorithm to find the correct nonce (hence validating the block)
+        validated_new_block     = blockchain.proofOfWork(non_validated_new_block)
+        print('VALIDATED BLOCK',  validated_new_block)
+
+        # try to add a block to the chain if it is valid
+        did_register_block = blockchain.registerBlock(validated_new_block)
+
+        # If successful reward the miner by adding a transaction granting the miner a coin
+        if did_register_block:
+            reward_transaction = {
+                'sender': 'MINING',
+                'receiver': node_id,
+                'amount': 1
+            }
+            blockchain.registerTransaction(reward_transaction)
+            print('WINNNING BLOCK MINED, 1 COIN RECEIVED')
+
+            block_index = validated_new_block['indx']
+            response = {
+                'message': f'Block {block_index} added to the blockchain',
+                'index': block_index,
+                'transactions': validated_new_block['transactions'],
+                'nonce': validated_new_block['nonce'],
+                'previous_hash': validated_new_block['previous_hash']
+            }
+            return jsonify(response), 200
+        else:
+            print('OTHER NODE MINED THE BLOCK, NO REWARD')
+            return jsonify({'message': 'Block is not valid, or other node finished first'}), 200
+
+
+
+@app.route('/transactions', methods=['GET'])
+def getPendingTransactions():
+    '''
+    - Returns all the pending transactions
+    '''
     response = {
-        'message': f'Block {block_index} added to the blockchain',
-        'index': block_index,
-        'transactions': validated_new_block['transactions'],
-        'nonce': validated_new_block['nonce'],
-        'previous_hash': validated_new_block['previous_hash']
+        'transactions': blockchain.pending_transactions,
+        'length': len(blockchain.pending_transactions)
     }
     return jsonify(response), 200
 
@@ -49,26 +123,40 @@ def mine():
 @app.route('/transactions/new', methods=['POST'])
 def newTransaction():
     '''
-    - Handles a request to register a new transaction in the pending block
+    - If seed node forward to participant nodes:
+    - else: register the transaction in the pending transactions
     '''
-    required_fields = ['sender', 'receiver', 'amount']
-    provided_fields = request.get_json()
-    print("New transaction received:", provided_fields)
+    if host_address == blockchain.seed_node_address:
+        print('I AM SEED NODE, FORWORDING THE REQUEST TO PARTICIPANT NODES')
+        # forward the request to all other nodes on the network
+        for address in blockchain.nodes:
+            if address == host_address:
+                continue
+            try:
+                requests.post(f'http://{address}/transactions/new', data=json.dumps(request.get_json()), headers={'Content-Type': 'application/json'})
+            except:
+                pass
+        return jsonify({'message': 'Request forwarded to all nodes on the network'}), 200
+    else:
+        print('I AM A PARTICIPANT NODE, REGISTERING TRANSACTION')
+        required_fields = ['sender', 'receiver', 'amount']
+        provided_fields = request.get_json()
 
-    # handle some types of bad requests
-    if not all(field in provided_fields for field in required_fields):
-        return 'Required data missing', 400
-    if not provided_fields['sender'] or type(provided_fields['sender']) != str:
-        return 'Invalid sender address', 400
-    if not provided_fields['receiver'] or type(provided_fields['receiver']) != str:
-        return 'Invalid receiver address', 400
-    if not provided_fields['amount'] or type(provided_fields['amount']) != int:
-        return 'Invalid amount', 400
+        # handle some types of bad requests
+        if not all(field in provided_fields for field in required_fields):
+            return 'Required data missing', 400
+        if not provided_fields['sender'] or type(provided_fields['sender']) != str:
+            return 'Invalid sender address', 400
+        if not provided_fields['receiver'] or type(provided_fields['receiver']) != str:
+            return 'Invalid receiver address', 400
+        if not provided_fields['amount'] or type(provided_fields['amount']) != int:
+            return 'Invalid amount', 400
 
-    # add the transaction to the pending block
-    block_indx = blockchain.registerTransaction(provided_fields)
-    response = {'message': f'Transaction added to pending block: {block_indx}'}
-    return jsonify(response), 201
+        # add the transaction to the pending block
+        block_indx = blockchain.registerTransaction(provided_fields)
+        print("transaction registered")
+        response = {'message': f'Transaction added to pending block: #{block_indx}'}
+        return jsonify(response), 201
 
 
 @app.route('/chain', methods=['GET'])
@@ -83,12 +171,26 @@ def chain():
     return jsonify(response), 200
 
 
+
+@app.route('/nodes', methods=['GET'])
+def getNodes():
+    '''
+    - Returns the nodes on the network
+    '''
+    response = {
+        'nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 200
+
+
+
 @app.route('/nodes/register', methods=['POST'])
 def registerNodes():
     ''''
     - Receives a list of new node addresses and register them on the netwok
     '''
     node_addresses = request.get_json().get('nodes')
+    print("New node joined the network:", node_addresses)
 
     # handle bad requests
     if not node_addresses:
@@ -126,4 +228,4 @@ def consensus():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=port)
